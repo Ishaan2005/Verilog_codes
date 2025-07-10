9744cfc1488d29a32d37d52ad2191b4ce79584e9
Ishaan2005 <ishaanbhimaji@gmail.com>
2025-07-10 08:28:28 +0530

    Testbench for Ripple carry adder

---
+++ Rippleadder_TB.v
@@ -0,0 +1,16 @@
+module DUT();
+reg a,b,cin;
+wire sum,cout;
+lpw xyz(a,b,cin,cout,sum);
+initial begin  
+    cin = 1; 
+    #5 a = 4'b0000; b = 4'b0011;
+    #5 a = 4'b0011; b = 4'b0101;
+    #5 a = 4'b0111; b = 4'b0011;
+    #5 a = 4'b0110; b = 4'b0100;
+    #5 a = 4'b0100; b = 4'b0101;
+    #5 a = 4'b0010; b = 4'b0011;
+    #5 a = 4'b0101; b = 4'b0101;
+    #5 a = 4'b0110; b = 4'b0001;
+  end
+endmodule
