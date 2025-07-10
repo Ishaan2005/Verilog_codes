306450dae700d343c39196be6da3809cd9f4c516
Ishaan Bhimajiyani <143024697+Ishaan2005@users.noreply.github.com>
2025-07-10 15:22:41 +0530

    Fulladder_TB.v

--- Fulladder_TB.v
+++ Fulladder_TB.v
@@ -20,3 +20,19 @@ for(i=0;i<10;i=i+1)begin
 end
 end
 endmodule
+
+module DUT();
+reg[2:0]A;
+reg[2:0]B;
+wire[3:0]sum;
+wire cout;
+reg cin;
+lpw xyz(A,B,sum,cout,cin);
+initial begin
+repeat(20)begin
+#5 A = $random%7;
+#5 B = $random%7;
+#5 cin = $random%2;
+end
+end
+endmodule
