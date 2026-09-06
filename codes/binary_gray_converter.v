2bb1767c514364fef92de6d848b649bdbdc935fc
BHIMAJIYANI ISHAAN KAUSHIKKUMAR <ishaanbhimaji@gmail.com>
2026-09-06 12:49:24 +0530

    new work for job switch

--- codes/binary_gray_converter.v
+++ codes/binary_gray_converter.v
@@ -8,3 +8,14 @@ endmodule
 module vlsi #(parameter k = 16)(input[k-1:0]in,output[k-1:0]out);
 assign out = in ^ (in >> 1);
 endmodule
+
+module DUT();
+reg[3:0]in;
+wire[3:0]out;
+lpw xyz(in,out);
+initial begin
+repeat(20)begin
+#5 in = $random%15;
+end
+end
+endmodule
