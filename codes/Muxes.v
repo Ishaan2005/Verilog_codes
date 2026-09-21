3a7715de0075922473bb2aaf731a63f86619ece4
BHIMAJIYANI ISHAAN KAUSHIKKUMAR <ishaanbhimaji@gmail.com>
2026-09-21 18:49:48 +0530

    new work for job switch

--- codes/Muxes.v
+++ codes/Muxes.v
@@ -41,9 +41,26 @@ assign out = in[sel];
 endmodule
 
 // parameterized mux
+
 module vlsi #(parameter k = 4)(input[2**k-1:0]in,output out,input[k-1:0]sel);
 assign out = in[sel];
 endmodule
 
-
+module Testbench;
+reg[15:0]in;
+reg[3:0]sel;
+wire out;
+vlsi v1(.in(in),.sel(sel),.out(out));
+initial begin
+$dumpfile("vlsi.vcd");
+$dumpvars("0,Testbench");
+$monitor($time,"in = %b,sel = %b,out = %b",in,sel,out);
+#5 in = 16'b1001_0101_1001_0111; sel = 4'b0;
+#5 sel = 4'b0100;
+#5 sel = 4'b0101;
+#5 sel = 4'b0110;
+#5 sel = 4'b1111;
+#5 $finish
+end
+endmodule 
 
